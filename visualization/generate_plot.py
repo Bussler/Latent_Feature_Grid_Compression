3ed02cd65772d0989f577e83d9a7f8236bb6665b
bussler <maarten.bussler@gmail.com>
2023-02-21 12:28:17 +0100

    - Add Plots for Hyperparameter Analysis

--- visualization/generate_plot.py
+++ visualization/generate_plot.py
@@ -159,8 +159,18 @@ def generateParetoFrontier():
 
 
 def HyperparamAnalysis():
-    BASENAME = 'experiments/hyperparam_search/mhd_p_NAS/200_WithFinetuning/mhd_p_200_'
-    experimentNames = np.linspace(0, 49, 50, dtype=int)
+    BASENAME = 'experiments/NAS/mhd_p_MaskStraightThrough/mhd_p_'  # 'experiments/NAS/mhd_p_MaskedStraightThrough_WithFinetuning_SetNWArchitecture/mhd_p_'
+    experimentNames = np.linspace(0, 79, 80, dtype=int)  # np.linspace(0, 49, 50, dtype=int)
+
+    #BASENAME = 'experiments/NAS/mhd_p_Smallify/mhd_p_'#'experiments/NAS/mhd_p_Smallify_WithFinetuning_SetNWArchitecture/mhd_p_'
+    #experimentNames = np.linspace(0, 79, 80, dtype=int)
+    # experimentNamesOther = [14,16,31,34,36,37,39,42,50,53,55,56,58,59,63,64,66,67,70,75,76]
+
+    #BASENAME = 'experiments/NAS/mhd_p_Variational_Dynamic_WithFinetuning_SearchNWArchitecture/mhd_p_'  # 'experiments/NAS/mhd_p_Variational_Dynamic_WithFinetuning_SetNWArchitecture/mhd_p_'
+    #experimentNames = np.linspace(0, 79, 80, dtype=int)  # np.linspace(0, 54, 55, dtype=int)
+
+    #BASENAME = 'experiments/NAS/mhd_p_Variational_Static_WithFinetuning_Buggy/mhd_p_'  # 'experiments/NAS/mhd_p_Variational_Static_SetNWArchitecture/mhd_p_Variational_Static_SetNWArchitecturemhd_p_'
+    #experimentNames = np.linspace(0, 79, 80, dtype=int)  # np.linspace(0, 44, 45, dtype=int)
 
     InfoName = 'info.txt'
     configName = 'config.txt'
@@ -181,10 +191,11 @@ def HyperparamAnalysis():
     paretoPsnr = []
     paretoLBeta = []
     paretoLWeight = []
-    paretoLR = []
-    paretoLGrad = []
-    paretoNLayer = []
-    paretoLRDecay = []
+    paretoMomentum = []
+    paretoThreshold = []
+
+    paretoDKLMult = []
+    paretoSigma = []
 
     for ppair in pareto_front:
         c = ppair[0]
@@ -194,85 +205,87 @@ def HyperparamAnalysis():
             cName = foldername + '/'+InfoName
 
             info = dict_from_file(cName)
-            if info['compression_ratio'] == c:
+            if info['compression_ratio'] == c and c < 1000:
                 config = dict_from_file(foldername+'/'+configName)
 
                 #pc = [c, config['lambda_betas'], config['lambda_weights'], config['lr'], config['grad_lambda'], config['n_layers'], config['lr_decay']]
                 paretoCompr.append(c)
                 paretoPsnr.append(p)
-                paretoLBeta.append(config['lambda_betas'])
-                paretoLWeight.append(config['lambda_weights'])
-                paretoLR.append(config['lr'])
-                paretoLGrad.append(config['grad_lambda'])
-                paretoNLayer.append(config['n_layers'])
-                paretoLRDecay.append(config['lr_decay'])
+                paretoLBeta.append(config['lambda_drop_loss'])
+                paretoLWeight.append(config['lambda_weight_loss'])
+                paretoMomentum.append(config['drop_momentum'])
+                paretoThreshold.append(config['drop_threshold'])
+
+                #paretoDKLMult.append(config['weight_dkl_multiplier'])
+                #paretoSigma.append(config['variational_sigma'])
 
     '''Plotting process'''
-    fig, ((ax1, ax2,ax3,ax4,ax5, ax6),(ax7,ax8,ax9, ax10,ax11,ax12)) = plt.subplots(2, 6, figsize=(15, 15), dpi= 100)
+    fig, (( ax3,ax4, ax6),( ax9, ax10, ax12)) = plt.subplots(2, 3, figsize=(13, 13), dpi= 200) #ax5, ax11,
 
-    ax1.plot(paretoLR,paretoCompr, label='LR', color = 'blue')
-    ax1.title.set_text('LR')
-    ax1.set_xlabel('lr')
-    ax1.set_ylabel('Compression Rate')
+    #ax1.plot(paretoDKLMult,paretoCompr, color = 'green')
+    #ax1.title.set_text('DKL Multiplier')
+    #ax1.set_xlabel('DKL Multiplier')
+    #ax1.set_ylabel('Compression Rate')
 
-    ax7.plot(paretoLR,paretoPsnr, label='LR', color = 'blue')
-    ax7.title.set_text('LR')
-    ax7.set_xlabel('lr')
-    ax7.set_ylabel('PSNR')
+    #ax7.plot(paretoDKLMult,paretoPsnr,  color = 'green')
+    #ax7.title.set_text('DKL Multiplier')
+    #ax7.set_xlabel('DKL Multiplier')
+    #ax7.set_ylabel('PSNR')
 
-    ax2.plot(paretoLGrad,paretoCompr, label='Lambda Gradient', color = 'green')
-    ax2.title.set_text('Lambda Gradient')
-    ax2.set_xlabel('Lambda Gradient')
-    ax2.set_ylabel('Compression Rate')
+    #ax2.plot(paretoSigma,paretoCompr, color = 'blue')
+    #ax2.title.set_text('pSigma')
+    #ax2.set_xlabel('pSigma')
+    #ax2.set_ylabel('Compression Rate')
 
-    ax8.plot(paretoLGrad,paretoPsnr, label='Lambda Gradient', color = 'green')
-    ax8.title.set_text('Lambda Gradient')
-    ax8.set_xlabel('Lambda Gradient')
-    ax8.set_ylabel('PSNR')
+    #ax8.plot(paretoSigma,paretoPsnr,  color = 'blue')
+    #ax8.title.set_text('pSigma')
+    #ax8.set_xlabel('pSigma')
+    #ax8.set_ylabel('PSNR')
 
-    ax3.plot(paretoLBeta,paretoCompr, label='Lambda Beta', color = 'orange')
-    ax3.title.set_text('Lambda Beta')
-    ax3.set_xlabel('Lambda Beta')
+    ax3.plot(paretoLBeta,paretoCompr, color = 'orange')
+    ax3.title.set_text('Lambda Droploss')
+    ax3.set_xlabel('Lambda Droploss')
     ax3.set_ylabel('Compression Rate')
 
-    ax9.plot(paretoLBeta,paretoPsnr, label='Lambda Beta', color = 'orange')
-    ax9.title.set_text('Lambda Beta')
-    ax9.set_xlabel('Lambda Beta')
+    ax9.plot(paretoLBeta,paretoPsnr,  color = 'orange')
+    ax9.title.set_text('Lambda Droploss')
+    ax9.set_xlabel('Lambda Droploss')
     ax9.set_ylabel('PSNR')
 
-    ax4.plot(paretoLWeight,paretoCompr, label='Lambda Weight', color = 'red')
-    ax4.title.set_text('Lambda Weight')
-    ax4.set_xlabel('Lambda Weight')
+    ax4.plot(paretoLWeight,paretoCompr,  color = 'red')
+    ax4.title.set_text('Lambda Weightloss')
+    ax4.set_xlabel('Lambda Weightloss')
     ax4.set_ylabel('Compression Rate')
 
-    ax10.plot(paretoLWeight,paretoPsnr, label='Lambda Weight', color = 'red')
-    ax10.title.set_text('Lambda Weight')
-    ax10.set_xlabel('Lambda Weight')
+    ax10.plot(paretoLWeight,paretoPsnr,  color = 'red')
+    ax10.title.set_text('Lambda Weightloss')
+    ax10.set_xlabel('Lambda Weightloss')
     ax10.set_ylabel('PSNR')
 
-    ax5.plot(paretoNLayer,paretoCompr, label='N Layer', color = 'violet')
-    ax5.title.set_text('N Layer')
-    ax5.set_xlabel('N Layer')
-    ax5.set_ylabel('Compression Rate')
+    #ax5.plot(paretoMomentum,paretoCompr, color = 'violet')
+    #ax5.title.set_text('Init Droprate')
+    #ax5.set_xlabel('Momentum')
+    #ax5.set_ylabel('Compression Rate')
 
-    ax11.plot(paretoNLayer,paretoPsnr, label='N Layer', color = 'violet')
-    ax11.title.set_text('N Layer')
-    ax11.set_xlabel('N Layer')
-    ax11.set_ylabel('PSNR')
+    #ax11.plot(paretoMomentum,paretoPsnr,  color = 'violet')
+    #ax11.title.set_text('Init Droprate')
+    #ax11.set_xlabel('Momentum')
+    #ax11.set_ylabel('PSNR')
 
-    ax6.plot(paretoLRDecay,paretoCompr, label='LR Decay', color = 'black')
-    ax6.title.set_text('LR Decay')
-    ax6.set_xlabel('LR Decay')
+    ax6.plot(paretoThreshold,paretoCompr,  color = 'black')
+    ax6.title.set_text('Threshold')
+    ax6.set_xlabel('Threshold')
     ax6.set_ylabel('Compression Rate')
 
-    ax12.plot(paretoLRDecay,paretoPsnr, label='LR Decay', color = 'black')
-    ax12.title.set_text('LR Decay')
-    ax12.set_xlabel('LR Decay')
+    ax12.plot(paretoThreshold,paretoPsnr,  color = 'black')
+    ax12.title.set_text('Threshold')
+    ax12.set_xlabel('Threshold')
     ax12.set_ylabel('PSNR')
 
     plt.legend()
-    filepath = 'plots/' + 'mhd_p_' + "200_Finetuning_" + 'HyperparamAnalyis' + '.png'
-    plt.savefig(filepath)
+    filepath = 'plots/LatexFigures/AnalyseHyperparam/' + 'mhd_p_' + "MaskedStraightThrough_" + 'SearchArch_HyperparamAnalyis'
+    plt.savefig(filepath + '.png')
+    tikzplotlib.save(filepath + '.pgf')
 
 
 def WeightHistogramm():
@@ -283,10 +296,10 @@ def WeightHistogramm():
     from model.model_utils import setup_model
     import torch
 
-    #Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Variational_Static/Unpruned_Variational_Static_1/config.txt'
-    #Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Variational_Dynamic/Unpruned_Variational_Dynamic_1/config.txt'
-    #Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Smallify/Unpruned_Smallify_1/config.txt'
-    Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Masked/Unpruned_Masked_1/config.txt'
+    #Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Variational_Static/Unpruned_Variational_Static_500/config.txt'
+    #Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Variational_Dynamic/Unpruned_Variational_Dynamic_500/config.txt'
+    #Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Smallify/Unpruned_Smallify_500/config.txt'
+    Configpath = 'experiments/Test_DiffDropratesPerLayer/mhd_p_Masked/Unpruned_Masked_500/config.txt'
     args = dict_from_file(Configpath)
 
     model = setup_model(args['d_in'], args['n_hidden_size'], args['d_out'], args['n_layers'], args['embedding_type'],
@@ -311,11 +324,12 @@ def WeightHistogramm():
         ax[i].hist(layers[i], bins=120, label=str(i))  # range=(-0.5, 0.5)
         ax[i].title.set_text('Layer '+str(i))
 
-    filepath = 'plots/LatexFigures/AnalyseNWWeights/' + 'mhd_p_' + 'MaskedStraightThrough_1_' + 'Weight_Historgramm'
+    filepath = 'plots/LatexFigures/AnalyseNWWeights/' + 'mhd_p_' + 'MaskedStraightThrough_500_' + 'Weight_Historgramm'
     plt.savefig(filepath + '.png')
     tikzplotlib.save(filepath + '.pgf')
 
 
 if __name__ == '__main__':
     #generateParetoFrontier()
-    WeightHistogramm()
+    #WeightHistogramm()
+    HyperparamAnalysis()
